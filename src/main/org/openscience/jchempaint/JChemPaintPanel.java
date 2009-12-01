ca105897f7f82a3ba002f88478e6153a95e10b98
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2009-12-01 13:23:19 +0000

    removed the reactions

--- src/main/org/openscience/jchempaint/JChemPaintPanel.java
+++ src/main/org/openscience/jchempaint/JChemPaintPanel.java
@@ -193,10 +193,10 @@ public class JChemPaintPanel extends AbstractJChemPaintPanel implements
             inputAdapter.setPopupMenu(ChemModel.class, new JChemPaintPopupMenu(
                     this, "chemmodel", this.guistring));
         }
-        if (inputAdapter.getPopupMenu(Reaction.class) == null) {
+        /*if (inputAdapter.getPopupMenu(Reaction.class) == null) {
             inputAdapter.setPopupMenu(Reaction.class, new JChemPaintPopupMenu(
                     this, "reaction", this.guistring));
-        }
+        }*/
     }
 
     /**
