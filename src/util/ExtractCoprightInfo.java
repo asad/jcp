71287855df1905fa1517602324ddcd3669346c5e
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2010-01-05 17:02:10 +0000

    the licence extraction handles multiple sections

--- src/util/ExtractCoprightInfo.java
+++ src/util/ExtractCoprightInfo.java
@@ -1,9 +1,15 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Date;
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 
 
@@ -22,13 +28,16 @@ public class ExtractCoprightInfo {
         File[] files = new File(args[0]).listFiles(new JarFileFilter());
         for(int i=0;i<files.length;i++){
             if(new File(files[i].getPath()+".meta").exists()){
-                Properties metaprops = new Properties();
-                metaprops.load(new FileInputStream(new File(files[i].getPath()+".meta")));
-                fos.write(new String(metaprops.getProperty("Library")+" "+metaprops.getProperty("Version")+" ("+metaprops.getProperty("Homepage")+")"+linesep).getBytes());
-                fos.write(new String("Copyright "+metaprops.getProperty("Copyright")+linesep).getBytes());
-                fos.write(new String("License: "+metaprops.getProperty("License")+" ("+metaprops.getProperty("LicenseURL")+")"+linesep).getBytes());
-                fos.write(new String("Download: "+metaprops.getProperty("Download")+linesep).getBytes());
-                fos.write(new String("Source available at: "+metaprops.getProperty("SourceCode")+linesep).getBytes());
+                Map<String,Map<String,String>> metaprops = readProperties(new File(files[i].getPath()+".meta"));
+                Iterator<String> itsect =metaprops.keySet().iterator();
+                while(itsect.hasNext()){
+                    String section = itsect.next();
+                    fos.write(new String(metaprops.get(section).get("Library")+" "+metaprops.get(section).get("Version")+" ("+metaprops.get(section).get("Homepage")+")"+linesep).getBytes());
+                    fos.write(new String("Copyright "+metaprops.get(section).get("Copyright")+linesep).getBytes());
+                    fos.write(new String("License: "+metaprops.get(section).get("License")+" ("+metaprops.get(section).get("LicenseURL")+")"+linesep).getBytes());
+                    fos.write(new String("Download: "+metaprops.get(section).get("Download")+linesep).getBytes());
+                    fos.write(new String("Source available at: "+metaprops.get(section).get("SourceCode")+linesep+linesep).getBytes());
+                }
             }
             if(new File(files[i].getPath()+".extra").exists()){
                 fos.write(new String("The author says:"+linesep).getBytes());
@@ -45,6 +54,33 @@ public class ExtractCoprightInfo {
     }
 
 
+    public static Map<String,Map<String,String>> readProperties(File file) throws IOException {
+        Map<String,Map<String,String>> props = new HashMap<String,Map<String,String>>();
+        Map<String, String> sectionProps = null;
+        FileInputStream fstream = new FileInputStream(file);
+        // Get the object of DataInputStream
+        DataInputStream in = new DataInputStream(fstream);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line;
+        //Read File Line By Line
+        while ((line = br.readLine()) != null)   {
+          if (line.startsWith("#")) {
+            // skip line
+          } else if (line.startsWith("[") && line.endsWith("]")) {
+            String section = line.substring(line.indexOf("[")+1, line.indexOf("]"));
+            sectionProps = new HashMap<String, String>();
+            props.put(section, sectionProps);
+          } else if (line.contains("=")) {
+            String[] keyValue = line.split("=");
+            if (sectionProps == null) {
+              System.err.println("ERROR: property without required section header");
+              System.exit(-1);
+            }
+            sectionProps.put(keyValue[0], keyValue[1]);
+          }
+        }
+        return props;
+      }
 }
 
 class JarFileFilter implements FileFilter
