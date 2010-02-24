e2c2a62cbd16309b62e81695e5ee2f6f185e5744
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2010-02-24 14:52:22 +0000

    the test did not work when running the whole class

--- src/test/org/openscience/jchempaint/JCPEditorAppletBugsTest.java
+++ src/test/org/openscience/jchempaint/JCPEditorAppletBugsTest.java
@@ -411,18 +411,19 @@ public class JCPEditorAppletBugsTest extends AbstractAppletTest{
         applet.click();
         applet.panel("renderpanel").robot.click(applet.panel("renderpanel").component(), new Point(100,100), MouseButton.LEFT_BUTTON,1);
         applet.button("eraser").click();
-        Point2d moveto=getAtomPoint(panel,5);
+        Point2d moveto=getAtomPoint(panel,0,1);
         applet.panel("renderpanel").robot.click(applet.panel("renderpanel").component(), new Point((int)moveto.x, (int)moveto.y), MouseButton.LEFT_BUTTON,1);
-        moveto=getAtomPoint(panel,4);
+        moveto=getAtomPoint(panel,0,1);
         applet.panel("renderpanel").robot.click(applet.panel("renderpanel").component(), new Point((int)moveto.x, (int)moveto.y), MouseButton.LEFT_BUTTON,1);
-        moveto=getAtomPoint(panel,3);
+        moveto=getAtomPoint(panel,0,1);
         applet.panel("renderpanel").robot.click(applet.panel("renderpanel").component(), new Point((int)moveto.x, (int)moveto.y), MouseButton.LEFT_BUTTON,1);
-        moveto=getAtomPoint(panel,2);
+        moveto=getAtomPoint(panel,0,1);
         applet.panel("renderpanel").robot.click(applet.panel("renderpanel").component(), new Point((int)moveto.x, (int)moveto.y), MouseButton.LEFT_BUTTON,1);
-        moveto=getAtomPoint(panel,1);
+        moveto=getAtomPoint(panel,0,1);
         applet.panel("renderpanel").robot.click(applet.panel("renderpanel").component(), new Point((int)moveto.x, (int)moveto.y), MouseButton.LEFT_BUTTON,1);
-        moveto=getAtomPoint(panel,0);
+        moveto=getAtomPoint(panel,0,1);
         applet.panel("renderpanel").robot.click(applet.panel("renderpanel").component(), new Point((int)moveto.x, (int)moveto.y), MouseButton.LEFT_BUTTON,1);
         Assert.assertEquals("C1CCCCC1", panel.getSmiles());
+        restoreModelToEmpty();
     }
 }
